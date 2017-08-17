b15a42b81fd5225a248e20ddb8770c39bf6f55b7
Eduardo Aparicio Cardenes <eduardo.aparicio.cardenes@gmail.com>
2017-08-17 10:13:28 +0200

    Routes with supertest require to be in an express app

--- backend/__tests__/routes.test.js
+++ backend/__tests__/routes.test.js
@@ -1,10 +1,17 @@
-const request = require('supertest')
+const request = require('supertest');
+const express = require('express');
+
 const routes = require('../routes');
 
-describe('Server application', () => {
-  it('GET / will return hello world', () => {
-    request(routes).get('/').then(data => {
-      expect(data.text).toBe('hello world!');
+let appRoutes;
+describe('API routes', () => {
+  beforeAll(() => {
+    appRoutes = express().use(routes);
+  })
+
+  it('should be server is accessible', () => {
+    request(appRoutes).get('/').then(data => {
+      expect(data.status).toBe(200);
     });
   });
 });
